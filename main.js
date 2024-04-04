66f0dc70da8ac13d945a68c1fc617658f33b195b
izzatillonemataliev <156827918+izzatillonemataliev@users.noreply.github.com>
2024-04-04 20:22:09 +0500

    Update main.js

--- main.js
+++ main.js
@@ -35,13 +35,13 @@ function getRandomColor() {
 
 
 function changeBackgroundColor2() {
-  var randomColor = getRandomColor();
+  let randomColor = getRandomColor();
   document.body.style.backgroundColor = randomColor;
 }
 
 function getRandomColor() {
-  var r = Math.floor(Math.random() * 256); // Random red value between 0 and 255
-  var g = Math.floor(Math.random() * 256); // Random green value between 0 and 255
-  var b = Math.floor(Math.random() * 256); // Random blue value between 0 and 255
-  return 'rgb(' + r + ',' + g + ',' + b + ')'; // Construct the RGB color
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255); 
+  return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
